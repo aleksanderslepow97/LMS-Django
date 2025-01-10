691f6dea447a4fe8949859dfcf1df111ad96f86b
Aleksander <sanya.slepow2015@yandex.ru>
2025-01-10 15:46:27 +0500

    LMSDJ_30.2_01

--- config/settings.py
+++ config/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
 
     'rest_framework',
     'users',
@@ -100,3 +101,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
+}
